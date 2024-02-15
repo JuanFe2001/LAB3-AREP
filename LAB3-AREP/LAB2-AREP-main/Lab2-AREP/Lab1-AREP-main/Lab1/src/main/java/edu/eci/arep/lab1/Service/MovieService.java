d61f8894c407ba6a09e348fb224c154006e77121
JuanFe2001 <123691538+JuanFe2001@users.noreply.github.com>
2024-02-14 22:01:20 -0500

    Add files via upload

---
+++ LAB3-AREP/LAB2-AREP-main/Lab2-AREP/Lab1-AREP-main/Lab1/src/main/java/edu/eci/arep/lab1/Service/MovieService.java
@@ -0,0 +1,16 @@
+package edu.eci.arep.lab1.Service;
+
+import java.io.IOException;
+import java.net.URI;
+
+
+/**
+ * Functional interface
+ * 
+ * @author Santiago Forero Yate
+ */
+public interface MovieService {
+    
+    public String handle(String message);
+}
+
